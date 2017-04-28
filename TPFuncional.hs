412d6b36cca0bcdfebc17bdbc301fb95f2442fd1
Guillermo Acuna <guillermod.acuna@gmail.com>
2017-04-28 15:59:16 -0300

    Agregadas bebidas -falta jarra loca

--- TPFuncional.hs
+++ TPFuncional.hs
@@ -26,5 +26,12 @@ masDeUnAmigo cliente = (length(amigos cliente)) > 1
 -- Punto 4: 
 reconocerAmigo cliente amigo
 	|(nombre cliente == nombre amigo) = error "No se puede ser amigo de uno mismo"
-	| any (nombre amigo) (amigos cliente) = error "Ya es amigo"
+	| elem (nombre amigo) (amigos cliente) = error "Ya es amigo"
 	| otherwise = UnCliente { (nombre cliente) (resistencia cliente) (amigos cliente):(nombre amigo)}
+	
+-- Punto 5:
+grogXD cliente = UnCliente {(nombre cliente) 0 (amigos cliente)}
+
+klusener gusto cliente = UnCliente {(nombre cliente) (resistencia cliente - length gusto) (amigos cliente)}
+
+tintico cliente = UnCliente {(nombre cliente) (resistencia cliente + length (amigos cliente) (amigos cliente)}
